c1b542c7bba5188aa997bd6c1ec53e77701f3dee
johnmartinezc <john.martinez@codeimmersives.com>
2022-10-24 15:36:15 -0400

    -a

---
+++ main.js
@@ -0,0 +1,23 @@
+let para = document.querySelector('p')
+para.style.color= 'lightblue'
+para.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+let header = document.querySelector("h1")
+header.style.fontSize = '48px'
+
+let item13 = document.querySelector('#item-13')
+item13.style.opacity = ".5"
+
+let item3 = document.querySelector('#item-3')
+item3.innerText = 'I say, "Hi!"'
+
+let myImage = document.querySelector('img')
+myImage.src = "https://cdn.vox-cdn.com/thumbor/WR9hE8wvdM4hfHysXitls9_bCZI=/0x0:1192x795/1400x1400/filters:focal(596x398:597x399)/cdn.vox-cdn.com/uploads/chorus_asset/file/22312759/rickroll_4k.jpg"
+myImage.style.height= '300px'
+
+let newImage = document.querySelector("#gotya")
+newImage.style.height= '300px'
+
+let item15 = document.querySelector('#item15')
+item15.className= 'item'
+
